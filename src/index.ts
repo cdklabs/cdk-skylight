468cf44f1b13344b0a8291f873a8c7852778a35a
Dudu (David) Twizer <dudut@amazon.com>
2022-01-16 22:34:29 +0200

    Adding Props as exported class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { WindowsNode } from "./windows-node";
-export { WindowsFSxMad } from "./windows-fsx-mad-r53";
-export { MadR53 } from "./mad-r53";
+export { WindowsNode, WindowsNodeProps } from "./windows-node";
+export { WindowsFSxMad, WindowsFSxMadProps } from "./windows-fsx-mad-r53";
+export { MadR53, MadR53Props } from "./mad-r53";
