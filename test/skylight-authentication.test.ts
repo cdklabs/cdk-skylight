511ec5359cdb0757173fc3d8e80439f6e26c0edd
Dudu Twizer <24310417+dudutwizer@users.noreply.github.com>
2022-02-17 02:30:17 +0200

    projen

--- test/skylight-authentication.test.ts
+++ test/skylight-authentication.test.ts
@@ -1,64 +1,64 @@
-import { App, aws_ec2, Stack } from "aws-cdk-lib";
-import { Secret } from "aws-cdk-lib/aws-secretsmanager";
-import * as skylight from "../src";
+import { App, aws_ec2, Stack } from 'aws-cdk-lib';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import * as skylight from '../src';
 
 const env = {
-	account: "1111111111",
-	region: "us-east-1",
+  account: '1111111111',
+  region: 'us-east-1',
 };
 const app = new App();
-const stack = new Stack(app, "test", { env: env });
-const vpc = new aws_ec2.Vpc(stack, "vpc", {});
+const stack = new Stack(app, 'test', { env: env });
+const vpc = new aws_ec2.Vpc(stack, 'vpc', {});
 
-test("authentication", () => {
-	const mad = new skylight.authentication.AdAuthentication(
-		stack,
-		"AdAuthentication",
-		{
-			vpc: vpc,
-		}
-	);
-	const mad2 = new skylight.authentication.AdAuthentication(
-		stack,
-		"AdAuthentication2",
-		{
-			vpc: vpc,
-			edition: "enterprise",
-			secret: new Secret(stack, "test-secret"),
-			domainName: "test-domain",
-			secretName: "custom-secret-name",
-			createWorker: false,
-		}
-	);
-	mad2.createServiceAccount("test1", "test2", "Test3");
-	const mad3 = new skylight.authentication.AdAuthentication(
-		stack,
-		"AdAuthentication3",
-		{
-			vpc: vpc,
-			edition: "enterprise",
-			secretName: "custom-secret-name",
-			ssmParameters: {
-				namespace: "custom-namespace",
-				secretPointer: "secret-pointer",
-				directoryIDPointer: "directory-pointer",
-			},
-			createWorker: false,
-		}
-	);
-	mad3.createADGroup("test", "test2");
-	mad.createADGroup("Test", "test2");
-	mad.createServiceAccount("test", "Test2", "test3");
-	expect(mad2).toHaveProperty(
-		"ssmParameters.namespace",
-		"cdk-skylight/authentication/mad"
-	);
-	expect(mad3).toHaveProperty(
-		"ssmParameters.namespace",
-		"custom-namespace/authentication/mad"
-	);
-	expect(mad).toHaveProperty(
-		"adObject.cfnResourceType",
-		"AWS::DirectoryService::MicrosoftAD"
-	);
+test('authentication', () => {
+  const mad = new skylight.authentication.AdAuthentication(
+    stack,
+    'AdAuthentication',
+    {
+      vpc: vpc,
+    },
+  );
+  const mad2 = new skylight.authentication.AdAuthentication(
+    stack,
+    'AdAuthentication2',
+    {
+      vpc: vpc,
+      edition: 'enterprise',
+      secret: new Secret(stack, 'test-secret'),
+      domainName: 'test-domain',
+      secretName: 'custom-secret-name',
+      createWorker: false,
+    },
+  );
+  mad2.createServiceAccount('test1', 'test2', 'Test3');
+  const mad3 = new skylight.authentication.AdAuthentication(
+    stack,
+    'AdAuthentication3',
+    {
+      vpc: vpc,
+      edition: 'enterprise',
+      secretName: 'custom-secret-name',
+      ssmParameters: {
+        namespace: 'custom-namespace',
+        secretPointer: 'secret-pointer',
+        directoryIDPointer: 'directory-pointer',
+      },
+      createWorker: false,
+    },
+  );
+  mad3.createADGroup('test', 'test2');
+  mad.createADGroup('Test', 'test2');
+  mad.createServiceAccount('test', 'Test2', 'test3');
+  expect(mad2).toHaveProperty(
+    'ssmParameters.namespace',
+    'cdk-skylight/authentication/mad',
+  );
+  expect(mad3).toHaveProperty(
+    'ssmParameters.namespace',
+    'custom-namespace/authentication/mad',
+  );
+  expect(mad).toHaveProperty(
+    'adObject.cfnResourceType',
+    'AWS::DirectoryService::MicrosoftAD',
+  );
 });
--- test/skylight-compute.test.ts
+++ test/skylight-compute.test.ts
@@ -1,63 +1,63 @@
-import { App, aws_ec2, Stack } from "aws-cdk-lib";
-import * as skylight from "../src";
+import { App, aws_ec2, Stack } from 'aws-cdk-lib';
+import * as skylight from '../src';
 
 const env = {
-	account: "1111111111",
-	region: "us-east-1",
+  account: '1111111111',
+  region: 'us-east-1',
 };
 const app = new App();
-const stack = new Stack(app, "test", { env: env });
-const vpc = new aws_ec2.Vpc(stack, "vpc", {});
+const stack = new Stack(app, 'test', { env: env });
+const vpc = new aws_ec2.Vpc(stack, 'vpc', {});
 
 const cluster = new skylight.compute.WindowsEKSCluster(
-	stack,
-	"ElasticCluster",
-	{
-		vpc: vpc,
-		eksSsmParameters: {},
-	}
+  stack,
+  'ElasticCluster',
+  {
+    vpc: vpc,
+    eksSsmParameters: {},
+  },
 );
 
-test("Skylight-WindowsNode", () => {
-	const windowsNodeObject = new skylight.compute.DomainWindowsNode(
-		stack,
-		"WindowsNode",
-		{
-			vpc: vpc,
-			userData: "hello",
-			madSsmParameters: {},
-		}
-	);
-	windowsNodeObject.runPsCommands(["echo hello world"], "hello");
+test('Skylight-WindowsNode', () => {
+  const windowsNodeObject = new skylight.compute.DomainWindowsNode(
+    stack,
+    'WindowsNode',
+    {
+      vpc: vpc,
+      userData: 'hello',
+      madSsmParameters: {},
+    },
+  );
+  windowsNodeObject.runPsCommands(['echo hello world'], 'hello');
 
-	windowsNodeObject.runPSwithDomainAdmin(["echo hello world"], "hello-withPS");
-	windowsNodeObject.openRDP("1.1.1.1/32");
-	expect(windowsNodeObject).toHaveProperty(
-		"instance.instance.cfnResourceType",
-		"AWS::EC2::Instance"
-	);
+  windowsNodeObject.runPSwithDomainAdmin(['echo hello world'], 'hello-withPS');
+  windowsNodeObject.openRDP('1.1.1.1/32');
+  expect(windowsNodeObject).toHaveProperty(
+    'instance.instance.cfnResourceType',
+    'AWS::EC2::Instance',
+  );
 });
 
 //EKS
-test("Skylight-WindowsEKSNodes", () => {
-	const nodes = new skylight.compute.WindowsEKSNodes(
-		stack,
-		"WindowsEKSNodesTest",
-		{
-			vpc: vpc,
-		}
-	);
+test('Skylight-WindowsEKSNodes', () => {
+  const nodes = new skylight.compute.WindowsEKSNodes(
+    stack,
+    'WindowsEKSNodesTest',
+    {
+      vpc: vpc,
+    },
+  );
 
-	nodes.addAdDependency({});
-	nodes.addEKSDependency(cluster.eksCluster);
-	nodes.addStorageDependency({}, {}, "folder");
-	nodes.addLocalCredFile({}, "group", "account");
-	expect(nodes).toHaveProperty(
-		"asgResource.cfnResourceType",
-		"AWS::AutoScaling::AutoScalingGroup"
-	);
+  nodes.addAdDependency({});
+  nodes.addEKSDependency(cluster.eksCluster);
+  nodes.addStorageDependency({}, {}, 'folder');
+  nodes.addLocalCredFile({}, 'group', 'account');
+  expect(nodes).toHaveProperty(
+    'asgResource.cfnResourceType',
+    'AWS::AutoScaling::AutoScalingGroup',
+  );
 });
 
-test("Skylight-WindowsEKSCluster", () => {
-	expect(cluster).toHaveProperty("eksCluster.clusterName");
+test('Skylight-WindowsEKSCluster', () => {
+  expect(cluster).toHaveProperty('eksCluster.clusterName');
 });
